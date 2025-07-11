287b484b188204b2fba9da43980175ccce32f9b3
wilsonli <little3201@163.com>
2025-07-11 08:16:09 +0800

    移除role对象定义

--- src/types.d.ts
+++ src/types.d.ts
@@ -58,26 +58,6 @@ export interface GroupPrivileges {
   actions?: string[]
 }
 
-export interface Role extends AudtiMetadata {
-  name: string
-  members?: string[]
-  enabled?: boolean
-  description?: string
-}
-
-export interface RoleMembers {
-  id: number
-  roleId: number
-  username: string
-}
-
-export interface RolePrivileges {
-  id: number
-  roleId: number
-  privilegeId: number
-  actions?: string[]
-}
-
 export interface Privilege extends AudtiMetadata {
   name: string
   superiorId?: number
