62955cd75ea9b544fbfd79378865dfde59fb9b0d
wilsonli <little3201@163.com>
2025-07-11 08:05:20 +0800

    优化图表组件，修改layout-padding为16px

--- src/components/ChartView.vue
+++ src/components/ChartView.vue
@@ -19,19 +19,16 @@ const props = withDefaults(defineProps<{
   height: '400px'
 })
 
-const options = computed(() => {
-  return Object.assign({}, props.options, {
-    theme: {
-      mode: $q.dark.isActive ? 'dark' : 'light'
-    }
-  })
-})
+const options = computed(() => ({
+  ...props.options,
+  theme: {
+    mode: $q.dark.isActive ? 'dark' : 'light'
+  }
+}))
 
 const elRef = ref<HTMLElement | null>(null)
 
 let chartRef: ApexCharts | null = null
-
-const contentEl = ref<Element>()
 
 const styles = computed(() => {
   const width = is.number(props.width) ? `${props.width}px` : props.width
@@ -58,7 +55,8 @@ watch(
   () => options.value,
   (options) => {
     if (chartRef) {
-      chartRef?.updateOptions(options, true, false) // 第二个参数 true 表示对图表强制更新
+      // 第二个参数 true 表示对图表强制更新
+      chartRef?.updateOptions(options, true, false)
     }
   },
   {
@@ -68,20 +66,13 @@ watch(
 
 const resizeHandler = debounce(() => {
   if (chartRef) {
-    chartRef?.destroy() // 销毁旧图表
-    initChart() // 重新初始化图表
+    chartRef.updateOptions(options.value, true, false)
   }
 }, 100)
 
-const contentResizeHandler = async (e: TransitionEvent) => {
+const contentResizeHandler = (e: TransitionEvent): void => {
   if (e.propertyName === 'width') {
     resizeHandler()
-  }
-}
-
-const handleContentResize = (e: TransitionEvent): void => {
-  if (e.propertyName === 'width') {
-    contentResizeHandler(e)
   }
 }
 
@@ -89,27 +80,25 @@ onMounted(() => {
   initChart()
 
   window.addEventListener('resize', resizeHandler)
-
-  contentEl.value = document.getElementsByClassName('q-page-container')[0]
-  if (contentEl.value) {
-    (contentEl.value as Element).addEventListener('transitionend', handleContentResize as (event: Event) => void)
+  if (elRef.value) {
+    (elRef.value).addEventListener('transitionend', contentResizeHandler as (event: Event) => void)
   }
 })
 
 onBeforeUnmount(() => {
   window.removeEventListener('resize', resizeHandler)
-  if (contentEl.value) {
-    (contentEl.value as Element).removeEventListener('transitionend', handleContentResize as (event: Event) => void)
+  if (elRef.value) {
+    (elRef.value as Element).removeEventListener('transitionend', contentResizeHandler as (event: Event) => void)
   }
 
   if (chartRef) {
-    chartRef.destroy() // 组件卸载时销毁图表
+    chartRef.destroy()
   }
 })
 
 onActivated(() => {
   if (chartRef) {
-    resizeHandler() // 图表重新激活时重新渲染
+    resizeHandler()
   }
 })
 </script>
