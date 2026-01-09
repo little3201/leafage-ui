bdd3023be4c8b65417f9366157615ff6c81ae474
wq li <little3201@163.com>
2026-01-09 10:03:57 +0800

    test: 移动msw配置文件到src/mocks目录

--- src/boot/msw-server.ts
+++ src/boot/msw-server.ts
@@ -4,10 +4,13 @@ import { handlers } from 'src/mocks'
 
 export default defineBoot(async ({ router }) => {
   // dev
-  if (!process.env.DEV) {
+  if (process.env.DEV) {
     const worker = setupWorker(...handlers)
     await worker.start({
-      onUnhandledRequest: 'bypass'
+      onUnhandledRequest: 'bypass',
+      serviceWorker: {
+        url: '/src/mocks/mockServiceWorker.js',
+      },
     })
 
     router.addRoute({
