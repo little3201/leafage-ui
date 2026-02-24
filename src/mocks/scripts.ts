fec16cb2a990d6618dc805432bb26bfab0109008
little3201 <little3201@163.com>
2026-02-24 09:42:12 +0800

    fix: Change params from array to object format

--- src/mocks/scripts.ts
+++ src/mocks/scripts.ts
@@ -8,7 +8,7 @@ const datas: Script[] = [
     name: 'MySQL',
     version: '8.0.34',
     os: 'LINUX',
-    params: [{ key: 'port', value: '3306' }],
+    params: { key: 'port', value: '3306' },
     body: '#!/bin/bash\necho \\"Starting PostgreSQL...\\"\nservice postgresql start\necho \\"PostgreSQL is now running.\\"\n',
     attachment: 'mysql-8.0.34-linux.tar.gz'
   }, {
@@ -16,7 +16,7 @@ const datas: Script[] = [
     name: 'Nginx',
     version: '1.8.12',
     os: 'WINDOWS',
-    params: [],
+    params: {},
     body: '#!/bin/bash\necho \\"Starting Nginx...\\"\nservice nginx start\necho \\"Nginx is now running.\\"\n',
     attachment: 'nginx-1.8.12-windows.zip'
   },
@@ -25,7 +25,7 @@ const datas: Script[] = [
     name: 'Nodejs',
     version: '20.5.6',
     os: 'WINDOWS',
-    params: [],
+    params: {},
     body: 'npm install',
     attachment: 'nodejs-20.5.6-windows.zip'
   },
@@ -34,7 +34,7 @@ const datas: Script[] = [
     name: 'PostgreSql',
     version: '16.2.3',
     os: 'LINUX',
-    params: [{ key: 'port', value: '5432' }],
+    params: { key: 'port', value: '5432' },
     body: '#!/bin/bash\necho \\"Starting PostgreSQL...\\"\nservice postgresql start\necho \\"PostgreSQL is now running.\\"\n',
     attachment: 'postgresql-16.2.3-linux.tar.gz'
   },
@@ -43,7 +43,7 @@ const datas: Script[] = [
     name: 'Redis',
     version: '6.0.1',
     os: 'WINDOWS',
-    params: [{ key: 'port', value: '6379' }],
+    params: { key: 'port', value: '6379' },
     body: '#!/bin/bash\necho \\"Starting Redis...\\"\nservice redis-server start\necho \\"Redis is now running.\\"\n',
     attachment: 'redis-6.0.1-windows.zip'
   },
@@ -52,7 +52,7 @@ const datas: Script[] = [
     name: 'Redis',
     version: '7.0.1',
     os: 'WINDOWS',
-    params: [{ key: 'port', value: '6379' }],
+    params: { key: 'port', value: '6379' },
     body: '#!/bin/bash\necho \\"Starting Redis...\\"\nservice redis-server start\necho \\"Redis is now running.\\"\n',
     attachment: 'redis-7.0.1-windows.zip'
   },
@@ -61,7 +61,7 @@ const datas: Script[] = [
     name: 'Redis',
     version: '3.0.1',
     os: 'WINDOWS',
-    params: [{ key: 'port', value: '6379' }],
+    params: { key: 'port', value: '6379' },
     body: '#!/bin/bash\necho \\"Starting Redis...\\"\nservice redis-server start\necho \\"Redis is now running.\\"\n',
     attachment: 'redis-3.0.1-windows.zip'
   }
