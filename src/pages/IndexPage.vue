475a3f8f768dc9f61cb40183a8c0d6e6a0e11926
wq li <little3201@163.com>
2026-05-27 13:46:13 +0800

    fix: 调整布局

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -8,9 +8,9 @@ import StatsWidget from 'components/dashboard/StatsWidget.vue'
 </script>
 
 <template>
-  <div class="bg-surface-50 dark:bg-surface-950 min-h-screen p-8 flex flex-col gap-6">
+  <div class="bg-surface-50 dark:bg-surface-950 min-h-screen flex flex-col">
     <AppTopbar />
-    <div class="flex flex-col w-full  gap-6 flex-1">
+    <div class="flex flex-col w-full p-6 gap-6 flex-1">
       <StatsWidget />
       <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
         <SalesTrendWidget />
