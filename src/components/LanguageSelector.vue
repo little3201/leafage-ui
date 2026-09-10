4af991f08cf951b8c9fa0d6d6d3ff5cca631af87
little3201 <little3201@163.com>
2026-09-10 14:15:23 +0800

    Bind locale to app store and update language function

--- src/components/LanguageSelector.vue
+++ src/components/LanguageSelector.vue
@@ -8,6 +8,7 @@ import { useAppStore } from "@/stores/app";
 
 const appStore = useAppStore();
 const { locale } = useI18n({ useScope: "global" });
+locale.value = appStore.locale;
 
 function changeLang(lang: string) {
   locale.value = lang;
